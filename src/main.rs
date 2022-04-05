4fe74c1381d388b9500b3e778fe5ef0a8bbb0680
John <johnrodonnell77@gmail.com>
2022-04-04 22:49:54 -0400

    Can now recieve messages. But every message. Cant handle user input.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
 use discord::model::Event as DiscordEvent;
 use discord::{Discord, State};
-use std::{io, env};
 use tui::{
     backend::{CrosstermBackend, Backend},
-    widgets::{Widget, Block, Borders},
+    widgets::{Widget, Block, Borders, Paragraph, List, ListItem},
     layout::{Layout, Constraint, Direction},
-    text::Text,
+    style::{Color, Modifier, Style},
+    text::{Text, Spans, Span},
     Terminal,
     Frame,
 };
+use std::{error::Error, io, env, thread, sync::mpsc};
 use crossterm::{
     event::{self, DisableMouseCapture, EnableMouseCapture, KeyCode, Event},
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
+
 
 enum InputMode {
     Normal,
@@ -21,9 +23,9 @@ enum InputMode {
 }
 
 struct App {
-    input: String,
-    input_mode: InputMode,
-    messages: Vec<String>,
+    input: String,          // Current value of the input box
+    input_mode: InputMode,  // Current input mode
+    messages: Vec<String>,  // Message History
 }
 
 impl Default for App {
@@ -36,7 +38,8 @@ impl Default for App {
     }
 }
 
-fn main() -> Result<(), io::Error>{
+fn main() -> Result<(), Box<dyn Error>>{
+
     // Setup Terminal
     enable_raw_mode()?;
     let mut stdout = io::stdout();
@@ -54,6 +57,11 @@ fn main() -> Result<(), io::Error>{
         LeaveAlternateScreen,
         DisableMouseCapture
     );
+
+    if let Err(err) = res {
+        println!("{:?}", err)
+    }
+
     Ok(())
 }
 
@@ -67,16 +75,55 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<(
     println!("Ready.");
 
     let mut state = State::new(ready);
+
+    let (tx, rx) = mpsc::channel();
+
+    let handle = thread::spawn(|| {
+        handle_discord(discord, connection, state, tx)
+    });
+
     loop {
-        println!("1");
+        // Draw the UI 
         terminal.draw(|f| ui(f, &app))?;
+
+        app.messages.push(rx.recv().unwrap());
+
+        // Check for input from user
         if let Event::Key(key) = event::read()? {
-            if let KeyCode::Char('q') = key.code {
-                println!("manas");
-                return Ok(());
+            match app.input_mode {
+                InputMode::Normal => match key.code {
+                    KeyCode::Char('i') => {
+                        app.input_mode = InputMode::Editing;
+                    }
+                    KeyCode::Char('q') => {
+                        return Ok(());
+                    }
+                    _ => {}
+                },
+                InputMode::Editing => match key.code {
+                    KeyCode::Enter => {
+                        app.messages.push(app.input.drain(..).collect());
+                    }
+                    KeyCode::Char(c) => {
+                        app.input.push(c);
+                    }
+                    KeyCode::Backspace => {
+                        app.input.pop();
+                    }
+                    KeyCode::Esc => {
+                        app.input_mode = InputMode::Normal;
+                    }
+                    _ => {}
+                },
             }
         }
-        println!("2");
+
+    }
+}
+
+fn handle_discord(discord: Discord, mut connection : discord::Connection, mut state: discord::State, a: mpsc::Sender<String>) -> io::Result<()> {
+
+    loop {
         let event = match connection.recv_event() {
             Ok(event) => event,
             Err(err) => {
@@ -91,33 +138,97 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<(
                 if let discord::Error::Closed(..) = err {
                     break;
                 }
-                println!("bust 2");
                 continue;
             },
         };
 
-        println!("3");
         state.update(&event); 
-        println!("4");
+
         match event {
             DiscordEvent::MessageCreate(message) => {
-                println!("bustisus");
                 if message.author.id == state.user().id {
                     continue;
                 }
-                println!("{}: {}", message.author.name, message.content);
+                a.send(message.content).unwrap();
             }
             _ => {}
         }
+
     }
-    return Ok(());
+
+    Ok(())
 }
 
 fn ui<B: Backend>(f: &mut Frame<B>, app: &App) {
-    let size = f.size();
-    let block = Block::default()
-        .title("Messages")
-        .borders(Borders::ALL);
-    f.render_widget(block, size);
+    let chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .margin(2)
+        .constraints(
+            [
+                Constraint::Length(1),
+                Constraint::Length(3),
+                Constraint::Min(1),
+            ]
+            .as_ref(),
+        )
+        .split(f.size());
+    
+    let (msg, style) = match app.input_mode {
+        InputMode::Normal => (
+            vec![
+                Span::raw("Press "),
+                Span::styled("q", Style::default().add_modifier(Modifier::BOLD)),
+                Span::raw(" to exit, "),
+                Span::styled("i", Style::default().add_modifier(Modifier::BOLD)),
+                Span::raw(" to start editing"),
+            ],
+            Style::default().add_modifier(Modifier::RAPID_BLINK),
+        ),
+        InputMode::Editing => (
+            vec![
+                Span::raw("Press "),
+                Span::styled("Esc", Style::default().add_modifier(Modifier::BOLD)),
+                Span::raw(" to stop editing, "),
+                Span::styled("i", Style::default().add_modifier(Modifier::BOLD)),
+                Span::raw(" to record the message"),
+            ],
+            Style::default(),
+        ),
+    };
 
+    let mut text = Text::from(Spans::from(msg));
+    text.patch_style(style);
+    let help_message = Paragraph::new(text);
+    f.render_widget(help_message, chunks[0]);
+
+    let input = Paragraph::new(app.input.as_ref())
+        .style(match app.input_mode {
+            InputMode::Normal => Style::default(),
+            InputMode::Editing => Style::default().fg(Color::Yellow),
+        })
+        .block(Block::default().borders(Borders::ALL).title("Input"));
+    f.render_widget(input, chunks[1]);
+
+    match app.input_mode {
+        InputMode::Normal => {}
+        InputMode::Editing => {
+            f.set_cursor(
+                chunks[1].x + app.input.len() as u16 + 1, 
+                chunks[1].y + 1,
+            )
+        }
+    }
+
+    let messages: Vec<ListItem> = app
+        .messages
+        .iter()
+        .enumerate()
+        .map(|(i, m)| {
+            let content = vec![Spans::from(Span::raw(format!("{}: {}", i, m)))];
+            ListItem::new(content)
+        })
+        .collect();
+
+    let messages = List::new(messages).block(Block::default().borders(Borders::ALL).title("Messages"));
+    f.render_widget(messages, chunks[2]);
 }
